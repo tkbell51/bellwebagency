fb9d6783ace35f0900ab47bd829cbea8583d9c60
Tim Bell <tkbell51@gmail.com>
2021-10-10 00:54:08 -0400

    changed radio to select

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -47,7 +47,7 @@
         />
       </div>
 
-      <div class="form__group text-white">
+      <!-- <div class="form__group text-white">
         <div class="form__radio--group">
           <input type="radio" class="form__radio-input" id="web-design" name="service" />
           <label for="web-design" class="form__radio-label">
@@ -69,14 +69,14 @@
             Brand Logo Design
           </label>
         </div>
-      </div>
-      <!-- <div class="form__group">
+      </div> -->
+      <div class="form__group">
 
-        <label for="services" class="form__label">Choose Service</label>
+        <label for="services" class="form__label">What type of service are you interested in?</label>
 
         <select name="service" id="service" class="form__select" required>
           <option value="n/a" disabled>
-            What type of service are you interested in?
+            Choose Service
           </option>
           <option value="Web Design">Web Design</option>
           <option value="Brand & Logo Desing">Brand &amp; Logo Design</option>
@@ -84,7 +84,7 @@
             Review Lead Reputation Management
           </option>
         </select>
-      </div> -->
+      </div>
 
       <div class="form__group">
         <label for="projectDescription" class="form__label"
@@ -215,6 +215,46 @@ export default {};
   //   visibility: hidden;
   //   transform: translateY(4rem);
   // }
+&__select {
+    display: block;
+    font-size: 16px;
+    font-family: sans-serif;
+    font-weight: 700;
+    color: #444;
+    line-height: 1.3;
+    padding: 0.6em 1.4em 0.5em 0.8em;
+    width: 100%;
+    box-sizing: border-box;
+    margin-bottom: 3rem;
+    border: 1px solid #aaa;
+    box-shadow: 0 1px 0 1px rgba(0, 0, 0, 0.04);
+    border-radius: 0.5em;
+    -moz-appearance: none;
+    -webkit-appearance: none;
+    appearance: none;
+    background-color: #fff;
+    background-image: url('data:image/svg+xml;charset=US-ASCII,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22292.4%22%20height%3D%22292.4%22%3E%3Cpath%20fill%3D%22%23007CB2%22%20d%3D%22M287%2069.4a17.6%2017.6%200%200%200-13-5.4H18.4c-5%200-9.3%201.8-12.9%205.4A17.6%2017.6%200%200%200%200%2082.2c0%205%201.8%209.3%205.4%2012.9l128%20127.9c3.6%203.6%207.8%205.4%2012.8%205.4s9.2-1.8%2012.8-5.4L287%2095c3.5-3.5%205.4-7.8%205.4-12.8%200-5-1.9-9.2-5.5-12.8z%22%2F%3E%3C%2Fsvg%3E'),
+      linear-gradient(to bottom, #ffffff 0%, #e5e5e5 100%);
+    background-repeat: no-repeat, repeat;
+    background-position: right 0.7em top 50%, 0 0;
+    background-size: 0.65em auto, 100%;
+    &::-ms-expand {
+      display: none;
+    }
+    &:hover {
+      border-color: #888;
+    }
+    &:focus {
+      border-color: #aaa;
+      box-shadow: 0 0 1px 3px rgba(59, 153, 252, 0.7);
+      box-shadow: 0 0 0 3px -moz-mac-focusring;
+      color: #222;
+      outline: none;
+    }
+    & option {
+      font-weight: normal;
+    }
+  }
 
    &__radio--group {
             // width: 30%;
